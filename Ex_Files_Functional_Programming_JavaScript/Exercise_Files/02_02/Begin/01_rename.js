dc057210329c6698f123fbf8f501b028b7363de8
SaraKing <sking@vertafore.com>
2018-11-13 15:25:23 -0700

    functinality added for ex files

--- Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/Begin/01_rename.js
+++ Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/Begin/01_rename.js
@@ -1,15 +1,14 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 02, Exercise 01
 
-console.log()
-console.log("Hello and welcome to the course!")
-console.log("We seem to be using")
-console.log("a lot of console.logs here.")
-console.log("Why don't we rename it")
-console.log("to make it look better?")
-console.log("To do that, simply assign the function")
-console.log("'console.log' to another variable.")
-console.log("You can call it whatever you want.")
-console.log("I called mine 'line' to reflect the")
-console.log("way that I'm using it.")
-console.log()
+var line = console.log
+line("Hello and welcome to the course!")
+line("We seem to be using")
+line("a lot of lines here.")
+line("Why don't we rename it")
+line("to make it look better?")
+line("To do that, simply assign the function")
+line("'line' to another variable.")
+line("You can call it whatever you want.")
+line("I called mine 'line' to reflect the")
+line("way that I'm using it.")
--- Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/Begin/02_debug.js
+++ Ex_Files_Functional_Programming_JavaScript/Exercise_Files/02_02/Begin/02_debug.js
@@ -1,8 +1,13 @@
 // Learning Functional Programming with Javascript
 // Chapter 02, Video 02, Exercise 02
+const DEBUG_MODE_ENABLED = false
 
-function debug(message) {
-  console.log("DEBUG: " + message)
+var debug
+
+if(DEBUG_MODE_ENABLED) {
+  debug = printDebugMessage
+} else {
+  debug = doNothing
 }
 
 // ...
@@ -11,8 +16,8 @@ debug("Some debug message")
 
 // ...
 
-// function printDebugMessage(message) {
-//   console.log("DEBUG: " + message)
-// }
-//
-// function doNothing() { }
+function printDebugMessage(message) {
+  console.log("DEBUG: " + message)
+}
+
+function doNothing() { }
